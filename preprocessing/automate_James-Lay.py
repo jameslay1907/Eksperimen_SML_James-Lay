d89a3600c8ce1b14b5ddff4b5990afe67e0b171a
jameslay1907 <jameslayjames@yahoo.com>
2026-06-03 14:39:19 +0700

    Updated Code

--- preprocessing/automate_James-Lay.py
+++ preprocessing/automate_James-Lay.py
@@ -37,7 +37,7 @@ def preprocess_data(data, target_column):
     X = data.drop(columns=[target_column])
     y = data[target_column]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
     # Scaling Numerical Features
     scaler = StandardScaler()
